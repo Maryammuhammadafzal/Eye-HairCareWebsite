1d538bb3f1deadc5f1442b649b9980473b443d8a
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-02-01 22:25:54 +0500

    website

--- src/assets/Pages/About/About.jsx
+++ src/assets/Pages/About/About.jsx
@@ -7,9 +7,12 @@ const About = () => {
   return (
      <section className="container mx-auto w-[100%] h-screen flex flex-col md:flex-row items-center">
      <div className="w-full md:w-1/2  p-6 flex flex-col gap-6">
-     <h2 className="text-7xl  font-bold relative text-gray-300 uppercase -mb-12 ">
-          Who We Are?
-        </h2>
+     <h1 className="text-6xl -mb-4 font-bold text-gray-300 relative ">
+          Who We Are
+          <span className="absolute  left-0  top-0  text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-[#30D5C8] opacity-40">
+            Who We Are
+          </span>
+        </h1>
        <h2 className="text-4xl font-bold z-10 text-gray-900">The Great Place Of Eyecare Hospital Center</h2>
        <p className="text-gray-600 mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit doloribus rerum maxime unde quasi nobis animi sit optio! Possimus culpa ab odio omnis voluptatum fugiat in itaque similique alias at!</p>
        <div className="mt-6 space-y-4">
