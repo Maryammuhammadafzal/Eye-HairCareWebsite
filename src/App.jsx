0836fff80fed5d5b376cbbfbeb668e1b5920b595
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-02-01 18:45:43 +0500

    website

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './assets/Pages/Home/Home'
-import Navbar from './assets/Components/Navbar'
+import { useState } from "react";
+import "./App.css";
+import Home from "./assets/Pages/Home/Home.jsx";
 
 function App() {
-
   return (
-
- <div data-layer="Main" className="Main w-[100%] h-auto relative ">
- <Home />
-</div> 
-
-  
-  )
+    <div data-layer="Main" className="Main w-[100%] h-auto">
+      <Home />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/assets/Pages/Home/Home.jsx
+++ src/assets/Pages/Home/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
        <Hero/>
        <Services/>
        <About/>
-       <Home/>
     </div>
   )
 }
